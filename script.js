9be9e907b24e7f7d859859c4ec97f8d57dcf5b9f
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-07-12 21:55:53 -0600

    Create playMove func for alternating player turns

--- script.js
+++ script.js
@@ -8,52 +8,75 @@ const board = (() => {
 })();
 
 
-
-
-
-
-
 const gameController = (() => {
 
-    /* 
-    > check active player - if one player active, other = inactive
-    > if player active, place marker - check if array location contains marker or zero
-    > if player has placed marker, switch players
-    */
+    console.log(board);
 
     function Player(marker) {
-        marker,
+        marker
+        /*
         placeMarker = (row, column) => {
             board[row].splice(column, 1, marker);
             console.log(board);
             switchPlayer();
             console.log(activePlayer);
-        };
-        return { marker, placeMarker };
+        }; */
+        return { marker /* placeMarker */ };
     }
 
     playerOne = Player("X");
     playerTwo = Player("O");
 
-    const players = [
-        { playerOne },
-        { playerTwo }
-    ]
-
+    const players = [ playerOne, playerTwo]
 
     let activePlayer = players[0];
 
+    showActivePlayer();
+
+    
+    // check active player - if one player active, other = inactive
+    // if player active, place marker - check if array location contains marker or zero
+    // if player has placed marker, switch players
+
+
     const switchPlayer = () => {
-       activePlayer = activePlayer === players[0] ? players[1] : players[0]
+       activePlayer = activePlayer === players[0] ? players[1] : players[0];
+       showActivePlayer();
+    }
+    
+    function showActivePlayer() {
+        activePlayer === players[0] ? console.log("Player One's turn.") : console.log("Player Two's turn.");
+        /*
+        if (activePlayer === players[0]) {
+            console.log("Player One's turn.");
+        } else {
+            console.log("Player Two's turn.")
+        };
+        */
     }
     
 
-    if (activePlayer === players[0]) {
-        console.log("Player One's turn.");
+    // only allow active player to play - condense into one function
+    // check for empty spots on board
+
+    function playMove(row, column) {
+        board[row].splice(column, 1, activePlayer.marker);
         console.log(board);
-    } else console.log("Player Two's turn.");
+        switchPlayer();
+        console.log(activePlayer);
 
-
+        // console.log('here')
+        // console.log(players[0].marker);
+        // console.log(activePlayer.marker);
+    } return { playMove }
+    /*
+    placeMarker = (row, column) => {
+        board[row].splice(column, 1, marker);
+        console.log(board);
+        switchPlayer();
+        console.log(activePlayer);
+    }
+    */
 
 })();
 
