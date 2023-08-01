d2c40045d96e3c2b3c99cca3fd19b4c4245d9657
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-07-31 21:52:38 -0600

    Reorganise game module

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
 "use strict"
 
-
-// gameEnd var -> end game if true --> function which executes to check game end? before starting new turn
-
-// end game message, ask user to restart ??
-
-// iterate through array to make sure all vals are NOT zero
-// if none are zero, check if there's no win - if none => draw
-
-
-// TO DO
-// gameEnd + reset defaults function to start new game OR just add button to reload page
-// change win types to factory functions????
-// remove console logs
-
-
-// LOGIC FOR UI VER
-
-// initialize gameboard
-// const cellList = document.querySelectorAll(".cell");
-
 const container = document.querySelector(".cell_container");
+const replayBtn = document.createElement('button');
 const text = document.createElement('div');
 const cellArray = [];
 let gameBoard = [ [], [], []];
@@ -34,7 +15,6 @@ const BoardCell = (value, marked, empty) => {
 
 // create board
 const showPage = (() => {
-
     
     text.setAttribute("id", "prompt");
     text.innerText = 'Player One, please place your marker';
@@ -49,7 +29,6 @@ const showPage = (() => {
         divCell.setAttribute("id", cellArray[i].value);
         container.appendChild(divCell);
     }
-    console.log(cellArray);
 
     // add to board array  so can check wins
     function addToBoard(index, num) {
@@ -60,20 +39,21 @@ const showPage = (() => {
     for (let i = 0; i < 3; i++) {
         // for (let j = 0; j < i; j++){
         addToBoard(0, i)
-        console.log(i);
     }
 
     for (let i = 3; i < 6; i++) {
         // for (let j = 0; j < i; j++){
         addToBoard(1, i)
-        console.log(i);
     }
 
     for (let i = 6; i < 9; i++) {
         // for (let j = 0; j < i; j++){
         addToBoard(2, i)
-        console.log(i);
     }
+    
+    replayBtn.setAttribute('class', 'replay');
+    replayBtn.innerText = 'Restart'
+    board.appendChild(replayBtn);
 
 })();
 
@@ -86,12 +66,10 @@ const Player = (playerName, marker) => {
             // change cell status to "filled"
             if (cellArray[cell.id].empty === true) {
                 cell.innerText = `${activePlayer.marker}`;
-                // delete console log later
-                console.log(activePlayer.marker);
 
                 cellArray[cell.id].empty = false;
                 cellArray[cell.id].marked = activePlayer.marker;
-                game.checkWinner();
+                game.gamePlay();
                 game.switchPlayer();
             } else alert('Please choose another spot!');     
         }; 
@@ -100,8 +78,7 @@ const Player = (playerName, marker) => {
 
 const createElements = (() => {
 
-    const cellList = document.querySelectorAll(".cell");
-   
+    const cellList = document.querySelectorAll(".cell");   
     
     cellList.forEach((cell) => {
         cell.addEventListener("click", () => {
@@ -112,145 +89,137 @@ const createElements = (() => {
     return { cellList }
 })();
 
+
 const playerOne = Player('Player One', 'X');
 const playerTwo = Player('Player Two', 'O');
 const players = [ playerOne, playerTwo ];
 let activePlayer = players[0];
 
 const game = (() => {
-
     let winner;
     let gameEnd = false;
 
-
     function switchPlayer() {
-    activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    text.innerText = activePlayer.playerName + '\'s turn'
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+        text.innerText = activePlayer.playerName + '\'s turn'
     }
 
-    // winning conditions
-    function checkWinner() {
-
+    const gamePlay = () => { //gameplay used to be called checkWinner
         let gameWin = false;
         let gameDraw = false;
 
-        const CheckWin = (arr) => {
-            const crossWin = arr => arr.every((obj) => {
-                return obj.marked === 'X';
-            });
-            const noughtWin = arr => arr.every((obj) => {
-                return obj.marked === 'O';
-            });
-            return { crossWin, noughtWin }
-        }
 
-        function noughtWinLoop(arr) {
-            for (let i = 0; i < gameBoard.length; i++) {
-                let result = CheckWin(arr[i]);
-                result = result.crossWin(arr[i])
-                if (result === true) {
-                    gameWin = true;
-                }
+        const checkWin = (() => {
+
+            const CheckWin = (arr) => {
+                const crossWin = arr => arr.every((obj) => {
+                    return obj.marked === 'X';
+                });
+                const noughtWin = arr => arr.every((obj) => {
+                    return obj.marked === 'O';
+                });
+                return { crossWin, noughtWin }
             }
-        }
-
-        function crossWinLoop(arr) {
-            for (let i = 0; i < gameBoard.length; i++) {
-                let result = CheckWin(arr[i]);
-                result = result.noughtWin(arr[i])
-                if (result === true) {
-                    gameWin = true;
-                }
-            }
-        }
-
-        // diagonal
-        (() => {
-            if (gameBoard[1][1].empty === false) {
-                if ((gameBoard[0][0].marked === gameBoard[1][1].marked) && (gameBoard[1][1].marked === gameBoard[2][2].marked)) {
-                    console.log('diag win');
-                    console.log(gameBoard);
-                    gameWin = true;
-                } else if ((gameBoard[2][0].marked === gameBoard[1][1].marked) && (gameBoard[1][1].marked === gameBoard[0][2].marked)) {
-                    console.log('diag win');
-                    gameWin = true;
-                }
-            }
-        })();
-
-
-        // vertical
-        (() => {
-
-            const col0 = []
-            const col1 = []
-            const col2 = []
-            
-            let cols = [col0, col1, col2];
-
-            // create array for each gameboard column and push into array
-            const createCol = (column, index)  => {
-                // iterate through array with for loop
-                for (let i = 0; i < gameBoard.length; i++) {
-                    column.push(gameBoard[i][index]);
-                }
-            }
-
-            // call function to push each column to col array
-            for (let i = 0; i < 3; i++) {
-                createCol(cols[i], [i]);
-            }
-            
-            crossWinLoop(cols);
-            noughtWinLoop(cols);
     
-        })();
-
-        
-        // horizontal
-        (() => {
-
-            crossWinLoop(gameBoard);
-            noughtWinLoop(gameBoard);
-
-        })();
-
-
-        // tie 
-        const checkMarked = arr => arr.every((obj) => {
-            return obj.empty === false;
-        })
-
-        let results = []
-        for (let i = 0; i < 3; i++) {
-            let result = checkMarked(gameBoard[i]);
-            results.push(result);
-        }
-
-        const checkTrue = arr => arr.every((val) => {
-            return val === true;
-        })
-
-        if (results.length === 3) {
-            let answer = checkTrue(results);
-            if (answer === true) {
-                if (gameWin === false) {
-                    gameDraw = true;
+            function noughtWinLoop(arr) {
+                for (let i = 0; i < gameBoard.length; i++) {
+                    let result = CheckWin(arr[i]);
+                    result = result.crossWin(arr[i])
+                    if (result === true) {
+                        gameWin = true;
+                    }
                 }
             }
-        }
+    
+            function crossWinLoop(arr) {
+                for (let i = 0; i < gameBoard.length; i++) {
+                    let result = CheckWin(arr[i]);
+                    result = result.noughtWin(arr[i])
+                    if (result === true) {
+                        gameWin = true;
+                    }
+                }
+            }
 
-        // declare winner
 
-        /* 
-        if marker = X -> P1 is winner
-        if marker = O -> P2 is winner
+            // diagonal
+            (() => {
+                if (gameBoard[1][1].empty === false) {
+                    if ((gameBoard[0][0].marked === gameBoard[1][1].marked) && (gameBoard[1][1].marked === gameBoard[2][2].marked)) {
+                        gameWin = true;
+                    } else if ((gameBoard[2][0].marked === gameBoard[1][1].marked) && (gameBoard[1][1].marked === gameBoard[0][2].marked)) {
+                        gameWin = true;
+                    }
+                }
+            })();
 
-        */
+
+            // vertical
+            (() => {
+
+                const col0 = []
+                const col1 = []
+                const col2 = []
+                
+                let cols = [col0, col1, col2];
+
+                // create array for each gameboard column and push into array
+                const createCol = (column, index)  => {
+                    // iterate through array with for loop
+                    for (let i = 0; i < gameBoard.length; i++) {
+                        column.push(gameBoard[i][index]);
+                    }
+                }
+
+                // call function to push each column to col array
+                for (let i = 0; i < 3; i++) {
+                    createCol(cols[i], [i]);
+                }
+                
+                crossWinLoop(cols);
+                noughtWinLoop(cols);
+        
+            })();
+
+            
+            // horizontal
+            (() => {
+
+                crossWinLoop(gameBoard);
+                noughtWinLoop(gameBoard);
+
+            })();
+            
+
+            // tie
+            (() => {
+                const checkMarked = arr => arr.every((obj) => {
+                    return obj.empty === false;
+                })
+        
+                let results = []
+                for (let i = 0; i < 3; i++) {
+                    let result = checkMarked(gameBoard[i]);
+                    results.push(result);
+                }
+        
+                const checkTrue = arr => arr.every((val) => {
+                    return val === true;
+                })
+        
+                if (results.length === 3) {
+                    let answer = checkTrue(results);
+                    if (answer === true) {
+                        if (gameWin === false) {
+                            gameDraw = true;
+                        }
+                    }
+                }
+            })();
+        })();
 
         const declareWinner = () => {
             activePlayer.marker === 'X' ? winner = playerOne : winner = playerTwo;
-            console.log('The winner is: ' + winner.playerName);
             text.remove();
             const declare = document.createElement('div');
             declare.setAttribute('id', 'prompt');
@@ -259,29 +228,38 @@ const game = (() => {
 
         }
 
+        const endGame = () => {
+            replayBtn.style.backgroundColor = 'var(--orange)';
+            replayBtn.style.color = 'white';
+            replayBtn.style.opacity = '1.0';
+
+
+            replayBtn.addEventListener('click', () => {
+                replayBtn.style.transform = 'translateY(3px)';
+                location.reload();
+            })
+
+            replayBtn.addEventListener('mouseenter', () => {
+                replayBtn.style.backgroundColor = '#F03C05';
+            })
+
+            replayBtn.addEventListener('mouseleave', () => {
+                replayBtn.style.backgroundColor = 'var(--orange)';
+            })
+        }
+    
         if (gameWin === true) {
-            console.log('winner chosen');
             declareWinner();
+            endGame();
             gameEnd = true;
         } else if (gameDraw === true) {
             console.log('it\'s a tie!');
+            text.innerText = 'It\s a tie!'
             text.innerText = 'Tie!'
             gameEnd = true;
         }
-
-        if (gameEnd === true) {
-            console.log('done!');
-        }
-    }
-    return { checkWinner, switchPlayer }
-})();
-
-
-function endGame() {
     
-    // can't remove event listeners on finish so maybejust delete the board?
-    // or place a message over the board so board can't be clicked lol
+    }
+    return {switchPlayer, gamePlay }
 
-    // reload page
-    location.reload();
-};
+})();
